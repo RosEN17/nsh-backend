d4f129423369b213e6feadd862c47137f3f55b3e
RosEN17 <71099619+RosEN17@users.noreply.github.com>
2026-03-26 18:25:50 +0100

    Fix CORS origin URL by removing trailing slash

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://nsh-frontend.vercel.app/"
+        "https://nsh-frontend.vercel.app"
         ],
     allow_credentials=True,
     allow_methods=["*"],
